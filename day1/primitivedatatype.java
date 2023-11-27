456657f1d353a1ff3ebf3d6e94f12c5482d91f04
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-11-27 19:31:53 +0530

    Create primitivedatatype.java

---
+++ day1/primitivedatatype.java
@@ -0,0 +1,17 @@
+public class primitivedata {
+    public static void main(String[] args) {
+        byte b = 127; 
+        System.out.println(b);
+        char rh = 'a';
+        System.out.println(rh);
+        boolean var = false;
+        System.out.println(var);
+        double price = 15.3;
+        System.out.println(price);
+        //long
+        //double
+        short n =20;
+        System.out.println(n);
+    }
+    
+}
