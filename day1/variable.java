48ee1e8cf0b5297372d191e5c127f0efaf0c0ce2
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-11-27 19:30:59 +0530

    Create variable.java

---
+++ day1/variable.java
@@ -0,0 +1,14 @@
+public class variable {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 4;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "tonny";
+        System.out.println(name);
+        a = b;
+        System.out.println(a);
+
+    }
+    
+}
