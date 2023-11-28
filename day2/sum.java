22a926480e0a72a85d9cc3f9230a776c57190b11
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-11-28 21:07:57 +0530

    Create sum.java

---
+++ day2/sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 45;
+        int sum = a + b;
+        System.out.println(sum);
+        
+    }
+    
+}
