900784887ca9b1b78b63b84ee89e18fa540e5a48
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-11-29 17:32:06 +0530

    Create typecasting.java

---
+++ day3/typecasting.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class typecasting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double marks = 234.23;
+        int marks1 = (int)marks;
+        System.out.println(marks1);
+    }
+    
+}
