000c503980c0e3d591295e87be8bc95a8348d888
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-11-29 17:33:27 +0530

    Create typepromotion.java

---
+++ day3/typepromotion.java
@@ -0,0 +1,11 @@
+public class typepramotion {
+    public static void main(String[] args) {
+        char a = 'a';
+        char b ='b';
+        System.out.println((int)(b));
+        System.out.println((int)(a));
+        System.out.println(b-a);
+    }
+    
+}
+//promotion rule 1
