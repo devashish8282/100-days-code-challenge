3dec247b2e8e6b062d2e3dc96488e5327fb01b27
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-11-29 17:34:30 +0530

    Create Typepromotion.java

---
+++ day3/Typepromotion.java
@@ -0,0 +1,10 @@
+public class Typepromotion {
+    public static void main(String[] args) {
+        long p = 234;
+        char a = 'a';
+        double d = 23.453;
+        System.out.println(p + a - d);
+    }
+    
+}
+//rule2
