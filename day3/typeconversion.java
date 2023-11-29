b9f97a9145bcb34e03395ee37684a8e34ab87525
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-11-29 17:31:06 +0530

    Create typeconversion.java

---
+++ day3/typeconversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class typeconversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float d = sc.nextInt();
+
+       // int a = 2;
+       // double b = a;
+        System.out.println(d);
+    }
+    
+}
