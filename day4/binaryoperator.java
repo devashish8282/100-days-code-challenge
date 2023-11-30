ad941d1df197396b6ffcd8826252332fa9e35094
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-11-30 21:06:35 +0530

    Create binaryoperator.java

---
+++ day4/binaryoperator.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class binaryoperator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        float c = a + b;
+        float d = a-b;
+        float e = a*b;
+        float f = a/b;
+        float g = a%b;
+        
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println("modulo =" +g);
+            
+             
+                
+    
+
+    }
+    
+}
