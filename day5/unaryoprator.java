9cde22678a980435f709fb9bb0c0ec56290c9c36
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-12-01 18:11:08 +0530

    Create unaryoprator.java

---
+++ day5/unaryoprator.java
@@ -0,0 +1,16 @@
+public class unaryoperator {
+    public static void main(String[] args) {
+        //pre increment 
+        //int a = 10;
+        //int b = ++a;
+       // System.out.println(a);
+        //System.out.println(b);
+        //post incremnt
+        int a = 10;
+        int b = a++;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+    
+}
