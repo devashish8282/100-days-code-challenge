0c9203a33c1d4f22fd360c131d49067ec98f620a
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-12-02 19:41:06 +0530

    Create logicaloperator.java

---
+++ day6/logicaloperator.java
@@ -0,0 +1,8 @@
+public class logicaloperator {
+    public static void main(String[] args) {
+        System.out.println((3>2)&&(5>1));//and gate
+         System.out.println((3<2)||(5<1));//or gate
+          System.out.println(!(3<2));//not gate
+    }
+    
+}
