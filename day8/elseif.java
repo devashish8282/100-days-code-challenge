1e741eba05e33207649078638468d4bc094afa5d
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-12-04 19:54:36 +0530

    Create elseif.java

---
+++ day8/elseif.java
@@ -0,0 +1,16 @@
+public class elseif {
+    public static void main(String[] args) {
+        int a = 11;
+        if(a>=18){
+            System.err.println("adult");
+        }
+        else if(a>=13){
+            System.out.println("teenagers");
+        }
+        else{
+            System.out.println("child");
+        }
+    }
+  
+  
+}
