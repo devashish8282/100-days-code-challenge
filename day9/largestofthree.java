d065a75ce82d46e78acdc08f01da8df5d6efd443
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-12-05 18:05:14 +0530

    Create largestofthree.java

---
+++ day9/largestofthree.java
@@ -0,0 +1,18 @@
+public class largestofthree {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c = 4;
+        if(a>b && a>c){
+            System.out.println(a);
+        }
+        else if(b>c){
+            System.out.println(b);
+        }
+        else{
+            System.out.println(c);
+        }
+
+    }
+    
+}
