8513936ba482cb769526ed51abcb868d51729249
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-12-05 18:06:48 +0530

    Create ternaryoperator.java

---
+++ day9/ternaryoperator.java
@@ -0,0 +1,9 @@
+public class ternaryoperatr {
+    public static void main(String[] args) {
+        int s = 4;
+        String type = ((s%2)== 0) ? "even" : "odd";
+        System.out.println(type);
+
+    }
+    
+}
