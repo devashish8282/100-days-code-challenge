eba967f5aa8335359cf135af412b0199f49d7173
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-12-06 23:24:32 +0530

    Create result.java

---
+++ day10/result.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class result {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int marks = sc.nextInt();
+        if(marks >=33){
+            System.out.println("pass");
+        }
+
+        else{
+            System.out.println("fail");
+        }
+    }
+    
+}
