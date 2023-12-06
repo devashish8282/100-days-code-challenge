69a9ddb6e8d82cc646d55f3ad1c45f0bba9c8b26
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-12-06 23:25:43 +0530

    Create switchorder.java

---
+++ day10/switchorder.java
@@ -0,0 +1,18 @@
+public class switchorder {
+    public static void main(String[] args) {
+        char ch = 'd';
+        switch (ch) {
+            case 'e': System.out.println("pakoda");
+                
+                break;
+                case 'd': System.out.println("panipurui");
+                break;
+                case 'f':System.out.println("samosa");
+                break;
+        
+            default: System.out.println("i am not hungry");
+                break;
+        }
+    }
+    
+}
