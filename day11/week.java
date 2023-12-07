7172c38417b377497e2e2897a468d30b8cb34885
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-12-07 21:14:45 +0530

    Create week.java

---
+++ day11/week.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class week {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int week = sc.nextInt();
+        switch (week) {
+            case 1 : System.out.println("sunday");
+                
+                break;
+                
+                
+                case 2 : System.out.println("monday");
+                
+                break;
+                case 3 : System.out.println("tuesday");
+                
+                break;
+                case 4 : System.out.println("wednesday");
+                
+                break;
+                case 5 : System.out.println("thursday");
+                
+                break;
+                case 6 : System.out.println("friday");
+                
+                break;
+                case 7 : System.out.println("saturday");
+                
+                break;
+
+        
+            default: System.out.println("no more day");
+                break;
+        }
+    }
+
+    
+}
