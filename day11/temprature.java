7ec14a9df2403ff33f2ac33aadba9f415800276e
Devashish Patil <140969377+devashish8282@users.noreply.github.com>
2023-12-07 21:13:58 +0530

    Create temprature.java

---
+++ day11/temprature.java
@@ -0,0 +1,13 @@
+public class temprature {
+    public static void main(String[] args) {
+        double temp = 112.2;
+        if (temp >= 100) {
+            System.out.println("you have a fever");
+            
+        } else {
+            System.out.println("you are healthy");
+            
+        }
+    }
+    
+}
